b4d72dd1b862811ecc433abbab4338404d64d9e6
Vladislav Maslakov <maslakov20@mail.ru>
2025-02-08 23:10:46 +0500

    Finish adaptive

--- src/helpers/burger.js
+++ src/helpers/burger.js
@@ -1,4 +1,5 @@
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.nav-bar').classList.toggle('flex');
+    document.querySelector('.open-button').classList.toggle('display-none');
 })
